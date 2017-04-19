1358fa7591bfddec1fcf7eeea9c90f185d47f140
Michael van Tellingen <michael@mvantellingen.nl>
2017-04-19 16:41:58 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('README.rst') as fh:
 
 setup(
     name='param-store',
-    version='0.0.1',
+    version='0.1.0',
     description="Parameter store for secrets",
     long_description=long_description,
     url='https://github.com/labd/python-param-store',
--- src/param_store/__init__.py
+++ src/param_store/__init__.py
@@ -3,4 +3,4 @@ from param_store.stores import *  # noqa
 
 __all__ = ['Env', 'EC2ParameterStore']
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
