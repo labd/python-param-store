31fc3d3a8fe0c72c578011ce9011b67e5f7a3471
Michael van Tellingen <michael@mvantellingen.nl>
2017-05-13 08:57:19 +0200

    Expand tests

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -8,16 +8,19 @@ from .utils import MockStore
 def test_interpolate_dict():
     my_dict = {
         'TEST_VAR': 'ok',
-        'TEST_DECRYPT': 'prefix-%{key-name}-data-%{missing}'
+        'TEST_DECRYPT': 'prefix-%{key-name}-data-%{missing}',
+        'TEST_DOT': '%{key.name}',
     }
 
     store = MockStore({
-        'key-name': 'secret'
+        'key-name': 'secret',
+        'key.name': 'geheim'
     })
     result = param_store.interpolate_dict(my_dict, store)
 
     assert result == {
         'TEST_VAR': 'ok',
-        'TEST_DECRYPT': 'prefix-secret-data-%{missing}'
+        'TEST_DECRYPT': 'prefix-secret-data-%{missing}',
+        'TEST_DOT': 'geheim',
     }
 
